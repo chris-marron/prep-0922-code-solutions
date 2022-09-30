2aed1b1e68f085fa724505b3018642451e2b8c41
Chris Marron <chrisluk04@gmail.com>
2022-09-30 00:30:55 +0000

    Completed prep-javascript-for-loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,12 @@
+// create your loops here.
+for (var i = 0; i <= 9; i++) {
+  console.log(i);
+}
+
+for (var j = 0; j <= 18; j += 2) {
+  console.log(j);
+}
+
+for (var k = 100; k > 0; k--) {
+  console.log('Time till Explosion: ', k);
+}
